47b70f20dff56b0d72cdb80d2eb439b5c8292981
BuiThanhBinh <binh.s078251@gmail.com>
2023-04-12 15:30:30 +0700

    add nav in header

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,23 +1,44 @@
 import logo from "./logo.png"
 import user from "./user.png"
 import cart from "./cart.png"
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import './Header.css'
 
 function Header(props) {
     return (
-        <div className="head">
-            <img className="img-left" src={logo} alt="logo"/>
-            
-            <div className="head-middle">
-                <span>About us</span>
-                <span>Our Product</span>
-                <span>Delivery</span>
-            </div>
+        <Navbar className="head">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img className="img-left" src={logo} alt="logo"/>
+                </Navbar.Brand>
+                
+                <Nav className="head-middle">
+                    <Nav.Link href="#About">About us</Nav.Link>
+                    <Nav.Link href="#Product">Our Product</Nav.Link>
+                    <Nav.Link href="#Home">Delivery</Nav.Link>
+                </Nav>
 
-            <img className="img-right" src={user} alt="user"/>
-            <img className="img-right" src={cart} alt="cart"/>
-            <input type="text" className="search-box img-right"></input>
-        </div>
+                <Navbar.Brand href="#User">
+                    <img className="img-right" src={user} alt="user"/>
+                </Navbar.Brand>
+                <Navbar.Brand href="#Cart">
+                    <img className="img-right" src={cart} alt="cart"/>
+                </Navbar.Brand>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2 search-box img-right"
+                    aria-label="Search"
+                  />
+                </Form>
+            </Container>
+        </Navbar>
     );
 }
 
